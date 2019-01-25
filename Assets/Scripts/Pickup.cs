a447a799ff2dd32438cac02c51c321335ba3b647
MudBox <37148223+MudBoxCode@users.noreply.github.com>
2019-01-25 22:25:03 +0100

    object destroy and variables

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public int water, maxWater, waterGain;
+    public int heat, maxHeat, heatGain;
+
+    void Start()
+    {
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Water")
+        {
+            Debug.Log("It Works"); 
+            Destroy(collision.gameObject);
+
+            water =+ waterGain; 
+            Debug.Log(water);
+
+        }
+
+
+    }
+
+
+}
