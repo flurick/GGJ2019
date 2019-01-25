4a631be68ee8de5699ee43f17a54e4b3889b1eaf
Mextrin <seba3552@hotmail.com>
2019-01-25 22:03:43 +0100

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float up = Input.GetAxis("Vertical");
+        float right = Input.GetAxis("Horizontal");
+
+        Vector3 movement = transform.rotation * new Vector3(-up, right);
+
+        transform.RotateAround(Vector3.zero, -movement, moveSpeed * Time.deltaTime);
+    }
+}
