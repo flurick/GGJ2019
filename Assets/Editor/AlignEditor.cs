e3158749d4f3873edca0dd1fcbd20dfd8a21a3d5
Mextrin <seba3552@hotmail.com>
2019-01-25 22:42:36 +0100

    Created editor folder

---
+++ Assets/Editor/AlignEditor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AlignWithSphere))]
+public class AlignEditor : Editor
+{
+    void OnSceneGUI()
+    {
+
+    }
+}
