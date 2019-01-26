35050b38c69e8183c22dd56df162984121c4a0a4
Adil <leugue-gamer@hotmail.com>
2019-01-26 13:38:49 +0100

    UI

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -3,34 +3,25 @@ using UnityEngine.UI;   //Required when using UI elements
 
 public class UI : MonoBehaviour
 {
-    private Image waterImage;
-    private Image warmthImage;
+    public Image waterImage;
+    public Image warmthImage;
 
-        [SerializeField]private float water;
-        [SerializeField]private float maxWater;
-        [SerializeField]private float heat;
-        [SerializeField]private float maxHeat;
-
-    public GameObject Pickup_script;
-    public Pickup Earth;
+    [SerializeField] private float water;
+    [SerializeField] private float maxWater;
+    [SerializeField] private float warmth;
+    [SerializeField] private float maxWarmth;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("UI script start");
-        Earth = Pickup_script.GetComponent<Pickup>();
 
-        water = Earth.water;
-        maxWater = Earth.maxWater;
-        heat = Earth.heat;
-        maxHeat = Earth.maxHeat;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Update fill amount
         waterImage.fillAmount = water/maxWater;
-        warmthImage.fillAmount = heat/maxHeat;
+        warmthImage.fillAmount = warmth/maxWarmth;
     }
 }
