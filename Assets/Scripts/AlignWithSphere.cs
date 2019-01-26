6a703840989e7b76dd9447206f98e064ae090ada
Mextrin <seba3552@hotmail.com>
2019-01-26 22:25:22 +0100

    Dynamic 2D projection

--- Assets/Scripts/AlignWithSphere.cs
+++ Assets/Scripts/AlignWithSphere.cs
@@ -7,11 +7,32 @@ public class AlignWithSphere : MonoBehaviour
 {
     public Vector3 centerRot = Vector3.forward;
 
+    Transform player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
 
     void Update()
     {
         transform.position = Quaternion.Euler(centerRot) * Vector3.forward * 55;
-        transform.eulerAngles = centerRot;
-        //transform.rotation = GameObject.Find("Player").transform.rotation;
+        //transform.eulerAngles = centerRot;
+        transform.rotation = GameObject.Find("Player").transform.rotation;
+
+        if (player)
+        {
+            Vector3 toPlayer = Vector3.Normalize(player.position);
+            float dotDistance = Vector3.Dot(toPlayer, transform.position);
+
+            if (dotDistance > 0)
+            {
+                float difference = player.position.magnitude - dotDistance;
+                Vector3 fixedPosition = transform.position + (toPlayer * difference);
+                transform.position = fixedPosition;
+            }
+        }
+
+
     }
 }
