76258c3e01cae45e8f6eff997f356457f7689dcb
Adil <leugue-gamer@hotmail.com>
2019-01-26 15:14:10 +0100

    La till lite checkar för errors i UI

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -20,18 +20,26 @@ public class UI : MonoBehaviour
         Debug.Log("UI script start");
 
         Earth = GameObject.Find("Player");
-        pickupScript = Earth.GetComponent<Pickup>();
+        if(Earth == null){
+            Debug.Log("UI could not find Player object");
+        }else{
+            pickupScript = Earth.GetComponent<Pickup>();
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        warmth = pickupScript.heat;
-        maxWarmth = pickupScript.maxHeat;
-        water = pickupScript.water;
-        maxWater = pickupScript.maxWater;
-
+        //Check if Player object exists
+        if(Earth != null){
+            warmth = pickupScript.heat;
+            maxWarmth = pickupScript.maxHeat;
+            water = pickupScript.water;
+            maxWater = pickupScript.maxWater;            
+        }else{
+            warmth = maxWarmth = water = maxWater = 100;
+        }
 
         waterImage.fillAmount = water / maxWater;
         warmthImage.fillAmount = warmth / maxWarmth;
