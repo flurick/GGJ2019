2a60b61ba2dac59633c8ba9a773604790e314285
MudBox <37148223+MudBoxCode@users.noreply.github.com>
2019-01-26 04:11:12 +0100

    Heat and water working

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    public int water, maxWater, waterGain;
-    public int heat, maxHeat, heatGain;
+    public float water, maxWater, waterGain, waterLoss;
+    public float heat, heatGain, heatLimit, heatLoss;
+    public bool heatToggle = false;
+
 
     void Start()
     {
@@ -13,18 +15,85 @@ public class Pickup : MonoBehaviour
 
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void Update()
     {
-        if (collision.gameObject.tag == "Water")
+        if (heatToggle && heat < heatLimit)
         {
-            Debug.Log("It Works"); 
-            Destroy(collision.gameObject);
+            heat += heatGain * Time.deltaTime;
+            water -= waterLoss * Time.deltaTime;
 
-            water =+ waterGain; 
-            Debug.Log(water);
+
+            Debug.Log("can you see me");
 
         }
 
+        else if (!heatToggle)
+        {
+            heat -= heatLoss * Time.deltaTime;
+        }
+
+        else
+        {
+            heatToggle = false;
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Star")
+        {
+            if (heatToggle == false && heat < heatLimit)
+            {
+                heatToggle = true;
+
+                if (heatToggle && heat >= heatLimit)
+                {
+                    heatToggle = false;
+                }
+
+            }
+
+            else
+            {
+                Debug.Log("can you see me");
+                heatToggle = false;
+            }
+        }
+
+        else if (collision.gameObject.tag == "Water")
+        {
+
+            Debug.Log("It Works");
+            Destroy(collision.gameObject);
+
+            water = +waterGain;
+            Debug.Log(water);
+
+
+
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Star")
+        {
+            if (heat > heatLimit)
+            {
+                heat = heatLimit;
+            }
+            heatToggle = false;
+
+        }
 
     }
 
