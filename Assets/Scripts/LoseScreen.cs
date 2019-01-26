ca74dca65e8bd82494a50a7c69c997bb5212567b
Adil <leugue-gamer@hotmail.com>
2019-01-26 22:14:23 +0100

    lose screen

--- Assets/Scripts/LoseScreen.cs
+++ Assets/Scripts/LoseScreen.cs
@@ -5,19 +5,16 @@ using System.Linq;
 
 public class LoseScreen : MonoBehaviour
 {
-    public float[] data1;
-    public float[] data2;
+
     // Start is called before the first frame update
     void Start()
     {
-        data1 = new float[24];
-        data2 = new float[24];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        data2 = data1.Concat(data2).ToArray();
-        data1 = data2.Concat(data2).ToArray();
+
     }
 }
